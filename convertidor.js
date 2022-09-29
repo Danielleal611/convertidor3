0e5303d497e5d825eb50c2b4683aa5610f9ff79e
Danielleal611 <danielantonioleal611@gmail.com>
2022-09-29 11:21:35 -0500

    nuevo

---
+++ convertidor.js
@@ -0,0 +1,15 @@
+function Convertir(){
+    var valor1 = parseInt(document.getElementById('valor1').value);
+    var resultado = valor1*4500;
+    document.getElementById('resultado').value=resultado
+    
+
+}
+
+function Convertir2(){
+    var valor2 = parseInt(document.getElementById('valor2').value);
+    var resultado = valor2/4500;
+    document.getElementById('resultado2').value=resultado
+    
+
+}
